50c47f021fe05a350af00c55cf8a946c0d938a50
Alaaeldeen Hamed <mregydev@gmail.com>
2023-12-27 18:38:15 +0100

    adding footer and  header to layout

---
+++ src/components/Header/HeaderLink.tsx
@@ -0,0 +1,34 @@
+import { ReactElement, ReactNode } from 'react';
+import { css } from '../../../styled-system/css';
+import Link from 'next/link';
+
+interface HeaderLinkProps {
+  children: ReactNode;
+  href: string;
+}
+
+const HeaderLink = ({ children, href }: HeaderLinkProps) => {
+  return (
+    <Link href={href}>
+      <div
+        className={css({
+          textDecoration: 'none',
+          fontWeight: 'bold',
+          color: 'white',
+          padding: 4,
+          cursor:'pointer',
+          _hover: {
+            bg: 'gray.100',
+            border: 2,
+            borderColor: 'white',
+            color: 'gray.600',
+          },
+        })}
+      >
+        {children}
+      </div>
+    </Link>
+  );
+};
+
+export default HeaderLink;
